b7c0f2015e2b82a60e2ab9099aebf49bf5dfcca9
HeliosAU <kohenbond223@gmail.com>
2026-04-27 22:21:18 +1000

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+let filesTotal = 0;
+let filesNeeded = 0;
+
+function GameDetails(servername, serverurl, mapname, maxplayers, steamid, gamemode, volume, language) {
+    document.getElementById('status').innerText = "Connecting to " + servername + " | Sector: " + mapname;
+}
+
+function SetFilesTotal(total) {
+    filesTotal = total;
+}
+
+function DownloadingFile(fileName) {
+    document.getElementById('status').innerText = "Decrypting: " + fileName;
+}
+
+function SetStatusChanged(status) {
+    document.getElementById('status').innerText = status;
+}
+
+function SetFilesNeeded(needed) {
+    filesNeeded = needed;
+    updateProgress();
+}
+
+function updateProgress() {
+    if (filesTotal > 0) {
+        let downloaded = filesTotal - filesNeeded;
+        let percentage = (downloaded / filesTotal) * 100;
+        document.getElementById('progress-fill').style.width = percentage + "%";
+        document.getElementById('files').innerText = downloaded + " / " + filesTotal;
+    }
+}
